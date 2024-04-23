0b618cc83287d5bcc3e13de51622748ea0c396c8
yasi98 <ypakdel@ucdavis.edu>
2024-04-23 14:53:51 -0700

    added hidden layer

--- Crypten-regression/model.py
+++ Crypten-regression/model.py
@@ -5,12 +5,15 @@ import torch.nn as nn
 class MLP(nn.Module):
     def __init__(self, input_size):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_size, 100)
-        self.fc2 = nn.Linear(100, 2)
+        self.fc1 = nn.Linear(input_size,128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 2)
 
     def forward(self, x: torch.Tensor):
-        res = self.fc1(x)
-        res = torch.relu(res)
-        res = self.fc2(res)
+        out = self.fc1(x)
+        out = torch.relu(out)
+        out = self.fc2(out)
+        out = torch.relu(out)
+        out = self.fc3(out)
         #res = res.view(-1)
-        return res
+        return out
